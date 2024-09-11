493cdbcfc428e1b18f763cf900087f1599dbf040
Rafael Luna <rafael.souza05@cs.unipe.edu.br>
2024-09-11 17:28:00 -0300

    iniciandosite

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,11 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Rafael Luna',
     })
-
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
